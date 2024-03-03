0c822e198658deea642443daeecfd88231118ade
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-03-03 13:41:00 +0900

    データベース作成

---
+++ src/test/java/in/techcamp/issueapp2/IssueApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
