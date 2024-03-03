b978b7b8d69179ec5cefabd590182b355bef4a1e
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2024-03-04 01:48:42 +0900

    削除機能実装

--- src/main/java/in/techcamp/issueapp2/IssueController.java
+++ src/main/java/in/techcamp/issueapp2/IssueController.java
@@ -1,11 +1,11 @@
 package in.techcamp.issueapp2;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,6 +22,32 @@ public class IssueController {
     @PostMapping("/issues")
     public String createIssue(IssueForm form){
         issueRepository.insert(form.getTitle(), form.getContent(), form.getPeriod(), form.getImportance());
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
+    }
+
+    @GetMapping("/issues/{id}")
+    public String issueDetail(@PathVariable long id, Model model){
+        var issue = issueRepository.findById(id);
+        model.addAttribute("issue",issue);
+        return "detail";
+    }
+
+    @PostMapping("/issues/{id}/update")
+    public String updateIssue(@PathVariable long id,IssueForm issueForm){
+        issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+        return "redirect:/";
+    }
+
+    @PostMapping("issues/{id}/delete")
+    public String deleteIssue(@PathVariable Long id){
+        issueRepository.deleteById(id);
         return "redirect:/";
     }
 }
